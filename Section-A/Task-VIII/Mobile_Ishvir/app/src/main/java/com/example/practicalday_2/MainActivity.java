3871bfcb3c0c3729ca953ae9c035c50957923dbf
Harpreet Singh <www.ishvir.1999@gmail.com>
2021-03-10 15:05:49 +0530

    Calculator (1706858)

---
+++ Section-A/Task-VIII/Mobile_Ishvir/app/src/main/java/com/example/practicalday_2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.practicalday_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("TASK 7");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}

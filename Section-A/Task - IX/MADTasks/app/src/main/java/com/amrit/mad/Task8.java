6b28603be6aa2b9ce75886ee395bac0e6fa7b4f6
Amrit Pal Singh <amrit121099@gmail.com>
2021-03-17 16:42:42 +0530

    1706827_Project

--- Section-A/Task - IX/MADTasks/app/src/main/java/com/amrit/mad/Task8.java
+++ Section-A/Task - IX/MADTasks/app/src/main/java/com/amrit/mad/Task8.java
@@ -1,13 +1,15 @@
 package com.amrit.mad;
 
-import androidx.appcompat.R;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,7 +46,7 @@ public class Task8 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task8);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setupUIViews();
 
         zero.setOnClickListener(new View.OnClickListener() {
@@ -236,5 +238,45 @@ public class Task8 extends AppCompatActivity {
         else{
             val1 = Double.parseDouble(info.getText().toString());
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.item_calculator).setVisible(false);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_linkages,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.item_home:
+                Intent intent = new Intent(getApplicationContext(),Task9.class);
+                startActivity(intent);
+                break;
+            case R.id.item_form:
+                Intent intent1 = new Intent(getApplicationContext(),Task6.class);
+                startActivity(intent1);
+                break;
+            case R.id.item_ui:
+                Intent intent2 = new Intent(getApplicationContext(),Task5.class);
+                startActivity(intent2);
+                break;
+            case R.id.item_signout:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

467760e6e5bff507a611c76e459d04767a58f2d4
ankitgitcode <07ankitsingh@gmail.com>
2021-03-17 19:41:01 +0530

    1706830

---
+++ Section-A/Task-VI/Task6/app/src/main/java/com/project/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.GridView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}

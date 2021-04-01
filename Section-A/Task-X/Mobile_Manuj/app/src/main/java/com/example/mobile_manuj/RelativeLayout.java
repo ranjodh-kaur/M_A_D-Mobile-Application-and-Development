92474a64429ac44a2778b30a91f7cee20c2220e7
Harpreet Singh <www.ishvir.1999@gmail.com>
2021-04-01 17:07:36 +0530

    Code

---
+++ Section-A/Task-X/Mobile_Manuj/app/src/main/java/com/example/mobile_manuj/RelativeLayout.java
@@ -0,0 +1,15 @@
+package com.example.mobile_manuj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+        getSupportActionBar().setTitle("Relative Layout");
+    }
+}
